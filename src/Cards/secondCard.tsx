da0a55ec0c80780f0d2614eb9776d37a2a20fd96
LeoSM_07 <leosmindlin@icloud.com>
2022-12-29 19:11:21 -0800

    Dark Mode, Start Comments, Input Validation

--- src/Cards/secondCard.tsx
+++ src/Cards/secondCard.tsx
@@ -1,51 +1,59 @@
-import styles from "../../styles/Home.module.css";
+import styles from "/styles/Home.module.css";
 import React, { useState } from 'react';
 
 interface FormData {
-    name: string;
-    email: string;
+  score: number;
+  maxScore: number;
+}
+interface FormProps {
+  onChange: (data: FormData) => void;
 }
 
-interface FormProps {
-    onChange: (data: FormData) => void;
-  }
-
+// The Form component is a functional component that takes in props of type FormProps
 export const SecondCard: React.FC<FormProps> = (props) => {
-    const [formData, setFormData] = useState<FormData>({
-        name: '',
-        email: '',
-      });
-    
-    //   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     setFormData({
-    //       ...formData,
-    //       [event.target.name]: event.target.value,
-    //     });
-    //     props.onChange(formData);
-    //   };
+  const [formData, setFormData] = useState<FormData>({
+    score: 0,
+    maxScore: 0,
+  });
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData({
-          ...formData,
-          [event.target.name]: event.target.value,
-        });
-        props.onChange({
-          ...formData,
-          [event.target.name]: event.target.value,
-        });
-      };
 
-    return (
-        <div className={styles.card}>
-            <div className={styles.cardHeader}>
-                <h1>Raw Grade</h1>
-            </div>
-            <hr />
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // Create a copy of the current form data
+    const updatedFormData = {
+      ...formData,
+      // Update the value of the field that was changed
+      [event.target.name]: event.target.valueAsNumber,
+    };
 
-            <form>
-                <input name="name" onChange={handleChange} />
-                <input name="email" onChange={handleChange} />
-            </form>
-        </div>
-    )
+    // Check if the score is greater than the input
+    // If it is, set the score equal to the input to prevent scores greater than 100%
+    if (event.target.name === "score" && updatedFormData.score > updatedFormData.maxScore) {
+      updatedFormData.score = updatedFormData.maxScore;
+    }
+
+    setFormData(updatedFormData);
+    props.onChange(updatedFormData);
+  };
+
+  // Check to see if input is less than 5 digits
+  // To prevent a super long number, messing up the layout
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length > 5) {
+      event.target.value = event.target.value.slice(0, 5);
+    }
+  };
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <h1>Raw Grade</h1>
+      </div>
+      <hr />
+
+      <form>
+        <input name="score" type="number" max={9999} maxLength={4} onChange={handleChange} onInput={handleInput} />
+        <input name="maxScore" type="number" max={9999} maxLength={4} onChange={handleChange} onInput={handleInput} />
+      </form>
+    </div>
+  )
 };
--- src/Cards/thirdCard.tsx
+++ src/Cards/thirdCard.tsx
@@ -1,22 +1,37 @@
-import styles from "../../styles/Home.module.css";
+import styles from "/styles/Home.module.css";
 
 interface DisplayProps {
     formData: {
-        name: string;
-        email: string;
+        score: number;
+        maxScore: number;
     };
 }
 
+
 export const ThirdCard: React.FC<DisplayProps> = (props) => {
+
     return (
         <div className={styles.card}>
             <div className={styles.cardHeader}>
                 <h1>Grade Output</h1>
             </div>
             <hr />
-            <p>Name: {props.formData.name}</p>
-            <p>Email: {props.formData.email}</p>
-            <div className={styles.bigText}>81%</div>
+            <p>Score: {props.formData.score}</p>
+            <p>Max Score: {props.formData.maxScore}</p>
+            <div className={styles.bigText}>{curveGrade(props.formData.score, props.formData.maxScore)}%</div>
         </div>
     )
 };
+
+function curveGrade(score: number, maxScore: number): string {
+    var curved: number
+
+    if (score > maxScore) {
+        score = maxScore
+    }
+
+    // console.log(score)
+
+    curved = ((score / maxScore)*100)
+    return ( curved.toFixed(2) )
+}
