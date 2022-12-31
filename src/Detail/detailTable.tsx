b5494cc682c309cd2c689acf54c54ab974716143
LeoSM_07 <leosmindlin@icloud.com>
2022-12-30 23:44:09 -0800

    Add Basics for Bottom Detail Section

--- src/Detail/detailTable.tsx
+++ src/Detail/detailTable.tsx
@@ -5,7 +5,7 @@ import { GradeChip } from "../Misc/chips";
 export const DetailRow = (props: DetailRowProp) => {
     return (
         <div>
-            <hr />
+            <hr style={{color: "var(--card-stroke-color)", opacity: 0.5}}/>
             <div className={styles.detailRow}>
                 <GradeChip text={props.finalGrade} hex={props.chipColor} />
                 <div style={{marginInline: "1rem"}}>{props.score}/{props.maxScore}</div>
