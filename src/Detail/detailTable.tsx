fff7fa30b9a8d6109c7307562c5dfe3f458d1eaf
Ben Ostrovsky <ben.leo.ost@gmail.com>
2023-01-03 17:52:31 -0800

    Turned Button Red

--- src/Detail/detailTable.tsx
+++ src/Detail/detailTable.tsx
@@ -2,8 +2,11 @@ import styles from "styles/Detail.module.css";
 import { DetailRowProp } from "./detailSection";
 import { GradeChip } from "../Misc/chips";
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import React from "react";
 
 export const DetailRow = (props: DetailRowProp) => {
+    const [color, setColor] = React.useState<string>('black');
+    const [fade, setFade] = React.useState<boolean>(false);
 
     const deleteFunction = () => {
         props.delete(props.index)
@@ -12,15 +15,29 @@ export const DetailRow = (props: DetailRowProp) => {
     return (
         <div className={styles.tableRow}>
             <hr style={{ color: "var(--card-stroke-color)", opacity: 0.3 }} />
-            <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                 <div className={styles.detailRow}>
                     <GradeChip text={props.finalGrade} hex={props.chipColor} />
                     <div style={{ marginInline: "1rem" }}>{props.score}/{props.maxScore}</div>
                 </div>
-                <button onClick={deleteFunction}>
+                <button
+                    onClick={deleteFunction}
+                    style={{
+                        color,
+                        transition: fade ? 'color 0.2s ease-in-out' : '',
+                    }}
+                    onMouseEnter={(event) => {
+                        setColor('red');
+                        setFade(true);
+                    }}
+                    onMouseLeave={(event) => {
+                        setColor('black');
+                        setFade(false);
+                    }}
+                >
                     <CancelOutlinedIcon></CancelOutlinedIcon>
                 </button>
-                
+
             </div>
         </div>
     )
